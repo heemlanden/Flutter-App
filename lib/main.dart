b716ade48a1a0dc62e149e1a5e1e8bdbf1902fff
Jurre <jurredijkstra@mijnheem.nl>
2025-09-11 14:36:59 +0200

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
         body: Center(
           child: Text('Hello World!'),
         ),
